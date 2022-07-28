a3d351ab4ea75227eadb941a2e71412ff12cc46a
Norgh1 <kippo1717@gmail.com>
2022-07-28 13:01:17 +0400

    added coordinator

---
+++ Chat-App/Coordinators/Coordinator.swift
@@ -0,0 +1,22 @@
+//
+//  Coordinator.swift
+//  Chat-App
+//
+//  Created by Nor Gh on 28.07.22.
+//
+
+import UIKit
+
+final class Coordinator {
+    
+    static func showOnboarding(from vc: UIViewController) {
+        let controller = vc.storyboard?.instantiateViewController(withIdentifier: OnboardingViewController.className)
+        vc.present(controller!, animated: true)
+    }
+    
+    static func showConversations(from vc: UIViewController) {
+        let controller = vc.storyboard?.instantiateViewController(withIdentifier: ConversationsViewController.className)
+        vc.present(controller!, animated: true)
+
+    }
+}
