d339ae9b3c9cbf1077805506b3958bbd659b77e8
Norgh1 <kippo1717@gmail.com>
2022-08-11 13:49:00 +0400

    I have worked in UserDefaults section. added @propartyWrapper

--- Chat-App/Services/UserDefaultsService.swift
+++ Chat-App/Services/UserDefaultsService.swift
@@ -5,20 +5,40 @@
 //  Created by Nor Gh on 28.07.22.
 //
 
-import Foundation
+import UIKit
 
-final class UserDefaultsService {
-	
-	
-	
+@propertyWrapper
+
+final class UserDefaultsService<T: Codable> {
+  
+  private var key: String
+  private let defaultValue: T
+  
+  init(key: String, defaultValue: T) {
+    self.key = key
+    self.defaultValue = defaultValue
+  }
+  
+  var wrappedValue: T {
+    get {
+      guard let data = UserDefaults.standard.object(forKey: key) as? Data else {
+      return defaultValue
+      }
+      let value = try? JSONDecoder().decode(T.self, from: data)
+      return value ?? defaultValue
+    }
+    set {
+      
+      let data = try? JSONEncoder().encode(newValue)
+      UserDefaults.standard.set(data, forKey: key)
+    }
+  }
 }
 
 //MARK: Model
 extension UserDefaults {
 	enum Key: String {
 		case isSignedin
+    case isSignup
 	}
 }
-
-
-
