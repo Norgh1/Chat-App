234866f4ac6f861a41e8ce80e48d78b02a92ce20
Henry Aslanyan <henry@wildlearner.com>
2022-08-16 19:04:55 +0400

    refactored conversation view controller

--- Chat-App/ViewControllers/ConversationsViewController.swift
+++ Chat-App/ViewControllers/ConversationsViewController.swift
@@ -8,54 +8,81 @@
 import UIKit
 
 final class ConversationsViewController: UIViewController {
-
-  let userModel : [UserModel] = [
-    UserModel(name: "first", image: UIImage(named: "emoji1")!),
-    UserModel(name: "first", image: UIImage(named: "emoji2")!),
-    UserModel(name: "first", image: UIImage(named: "emoji3")!),
-    UserModel(name: "first", image: UIImage(named: "emoji4")!),
-    UserModel(name: "first", image: UIImage(named: "emoji1")!),
-    UserModel(name: "first", image: UIImage(named: "emoji2")!),
-    UserModel(name: "first", image: UIImage(named: "emoji3")!),
-    UserModel(name: "first", image: UIImage(named: "emoji4")!)
-  ]
   
-  @IBOutlet weak var collectionViewTop: UICollectionView!
-    
+  //MARK: Outlets
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  //MARK: Private properties
+  private var users = [ObjectUser]()
+  private var conversations = [ObjectConversation]()
+  private var state: Mode = .inital {
+    didSet { updateUI() }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureCollectionViewTop()
-  }
-  
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-  }
-
-  @IBAction func logoutPressed(_ sender: Any) {
-    Usermanager.shared.logout { _ in
-      Coordinator.showOnboarding()
-    }
+    state = .inital
+    observeUsers()
+    observeConversations()
   }
 }
 
-extension ConversationsViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//MARK: Private methods
+private extension ConversationsViewController {
+  func observeUsers() {
+    
+  }
+  
+  func observeConversations() {
+    
+  }
+  
+  func updateUI() {
+    
+  }
+}
+
+//MARK: CollectionView methods
+extension ConversationsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    userModel.count
+    switch state {
+      case .inital, .networkError, .noItems:
+        return 1
+      case .normal:
+        return conversations.count + 1
+    }
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ConversationCell.id, for: indexPath) as! ConversationCell
-    cell.configure(with: userModel[indexPath.row])
-    return cell
+    switch state {
+      case .inital, .networkError, .noItems:
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PlaceholderCell.className, for: indexPath) as! PlaceholderCell
+        //TODO configure cell
+        return cell
+      case .normal:
+        guard indexPath.row != 0 else {
+          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UsersCell.className, for: indexPath) as! UsersCell
+          return cell.configure(users)
+        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ConversationCell.className, for: indexPath) as! ConversationCell
+        return cell.configure(conversations[indexPath.row - 1])
+    }
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    guard state == .normal, indexPath.row != 0 else { return }
+    //TODO
+    let conversation = conversations[indexPath.row - 1]
   }
 }
 
+
+//MARK: Models
 private extension ConversationsViewController {
-  func configureCollectionViewTop() {
-    collectionViewTop.dataSource = self
-    collectionViewTop.delegate = self
-    collectionViewTop.register(ConversationCell.self, forCellWithReuseIdentifier: ConversationCell.id)
-    collectionViewTop.showsHorizontalScrollIndicator = false
-    collectionViewTop.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+  enum Mode {
+    case inital
+    case noItems
+    case normal
+    case networkError
   }
 }
