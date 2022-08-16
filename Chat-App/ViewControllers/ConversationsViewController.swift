589eda9a9ee5a2c8517b0573866ee726f9dde6a2
Norgh1 <kippo1717@gmail.com>
2022-08-16 15:40:05 +0400

    added conversation uicollection

--- Chat-App/ViewControllers/ConversationsViewController.swift
+++ Chat-App/ViewControllers/ConversationsViewController.swift
@@ -7,53 +7,55 @@
 
 import UIKit
 
-final class ConversationsViewController: UIViewController{
+final class ConversationsViewController: UIViewController {
+
+  let userModel : [UserModel] = [
+    UserModel(name: "first", image: UIImage(named: "emoji1")!),
+    UserModel(name: "first", image: UIImage(named: "emoji2")!),
+    UserModel(name: "first", image: UIImage(named: "emoji3")!),
+    UserModel(name: "first", image: UIImage(named: "emoji4")!),
+    UserModel(name: "first", image: UIImage(named: "emoji1")!),
+    UserModel(name: "first", image: UIImage(named: "emoji2")!),
+    UserModel(name: "first", image: UIImage(named: "emoji3")!),
+    UserModel(name: "first", image: UIImage(named: "emoji4")!)
+  ]
   
-    @IBOutlet weak var collectionView: UICollectionView!
+  @IBOutlet weak var collectionViewTop: UICollectionView!
     
-  
-  
   override func viewDidLoad() {
     super.viewDidLoad()
-      collectionView.backgroundColor = .systemGray5
-      navigationItem.title = "xChat"
+    configureCollectionViewTop()
   }
   
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
   }
-  
-  
+
   @IBAction func logoutPressed(_ sender: Any) {
-    Usermanager.shared.logout { status in
-      guard case .success = status else {
-        return
-      }
+    Usermanager.shared.logout { _ in
       Coordinator.showOnboarding()
     }
   }
-
 }
 
-extension ConversationsViewController : UICollectionViewDelegate, UICollectionViewDataSource {
-  
+extension ConversationsViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
+    userModel.count
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let collView = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
-    collectionView.backgroundColor = .systemGray5
-    return collView
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ConversationCell.id, for: indexPath) as! ConversationCell
+    cell.configure(with: userModel[indexPath.row])
+    return cell
   }
-    
-    
-    
 }
 
-
-
-
-
-
-
+private extension ConversationsViewController {
+  func configureCollectionViewTop() {
+    collectionViewTop.dataSource = self
+    collectionViewTop.delegate = self
+    collectionViewTop.register(ConversationCell.self, forCellWithReuseIdentifier: ConversationCell.id)
+    collectionViewTop.showsHorizontalScrollIndicator = false
+    collectionViewTop.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+  }
+}
--- Chat-App/Views/ConversationCell.swift
+++ Chat-App/Views/ConversationCell.swift
@@ -8,34 +8,54 @@
 import UIKit
 
 final class ConversationCell : UICollectionViewCell  {
-//    
-//    //MARK: Static
-//    
-//    static let identifire = "ConversationCell"
-//    
-//    //MARK: Private proparties
-//    
-//    private let imageLabel: UILabel = {
-//       let label = UILabel()
-//        label.text = "text"
-//        label.backgroundColor = .blue
-//        label.textColor = .systemBlue
-//        label.contentMode = .center
-//        return label
-//    }()
-//    
-//	override init(frame: CGRect) {
-//		super.init(frame: frame)
-//		contentView.backgroundColor = .systemGray5
-//        contentView.addSubview(imageLabel)
-//	}
-//	
-//	required init?(coder: NSCoder) {
-//		fatalError("init(coder:) has not been implemented")
-//	}
-//    
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//        imageLabel.frame = self.contentView.bounds
-//    }
+    
+  static let id = "ConversationCell"
+  
+  
+  
+  
+  
+  let myImageView : UIImageView = {
+    let imageView = UIImageView()
+    imageView.clipsToBounds = true
+    imageView.contentMode = .scaleAspectFill
+    
+    imageView.layer.masksToBounds = true
+    imageView.layer.cornerRadius = 80 / 2
+    imageView.backgroundColor = .clear
+    return imageView
+  }()
+  
+
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    contentView.clipsToBounds = true
+    contentView.addSubview(myImageView)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override class func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    myImageView.frame = contentView.bounds
+  }
+  
+}
+
+extension ConversationCell {
+  func configure(with name: UserModel) {
+    myImageView.image = name.image    
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    myImageView.image = nil
+  }
 }
