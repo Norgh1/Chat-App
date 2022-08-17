598e2f60128adf26a88407fc4f5c047726051f2e
Norgh1 <kippo1717@gmail.com>
2022-08-18 02:19:46 +0400

    added conversation delegate to ConversationCell

--- Chat-App/Cells/ConversationCell.swift
+++ Chat-App/Cells/ConversationCell.swift
@@ -7,14 +7,25 @@
 
 import UIKit
 
+//MARK: Delegate
+protocol ConversationCellDelegate: UIViewController {
+  func didiSelect(_ conversation: ObjectConversation)
+}
+
 final class ConversationCell: UICollectionViewCell {
   
-//  @IBOutlet weak var userView: UserView!
-//  @IBOutlet weak var userNameLabel: UILabel!
-//  @IBOutlet weak var lastMessage: UILabel!
+  //MARK: Private proparies
+  private var user = ObjectUser()
+  
+  @IBOutlet weak var userView: UserView!
+  @IBOutlet weak var userNameLabel: UILabel!
+  @IBOutlet weak var lastMessage: UILabel!
   
     
   func configure(_ conversation: ObjectConversation) -> ConversationCell {
+    userNameLabel.text = conversation.id
+    lastMessage.text = conversation.lastMessage
+    userView.user = user
     return self
   }
  
