39708c62b0752a697d7a161ce21b97d3e4b552fd
Norgh1 <kippo1717@gmail.com>
2022-08-19 15:24:12 +0400

    added featchAllUsers method in ComposeViewController

--- Chat-App/ViewControllers/UserComposeViewController.swift
+++ Chat-App/ViewControllers/UserComposeViewController.swift
@@ -6,20 +6,34 @@
 //
 
 import UIKit
+import FirebaseFirestore
+
 
 final class UserComposeViewController: UIViewController {
+  
+  //MARK: Private properties
+  private var user = ObjectUser()
+  private var conversation = [ObjectConversation]()
+  private var servise = FirestoreService()
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    Usermanager.shared.fetchAllUsers { response in
-      //users
+    Usermanager.shared.fetchAllUsers { reponse  in
+      self.servise.read(type: ObjectUser.self, path: .conversations) { response in
+        switch response {
+        case.generalError, .noConnection:
+          break
+        case.success(let currentUser):
+          guard let allUsers = currentUser?.filter({$0.id != self.user.id }) else { return }
+          print("Print meee! \(allUsers.count)")
+        }
+      }
+    }
       ConversationManager.shared.fetchConversations { response in
+        }
         //check if selected user already has a conversation
         //if so show messages viewconroller
         //otherwise create conversation and then show the messages viewconroller
       }
     }
-  }
-  
-}
