6ab893ee94e5e5ed3e44d6d998752c1f86f1962b
Norgh1 <kippo1717@gmail.com>
2022-08-19 16:40:00 +0400

    chacked if there is contains conversation?

--- Chat-App/ViewControllers/MessagesViewController.swift
+++ Chat-App/ViewControllers/MessagesViewController.swift
@@ -8,15 +8,24 @@
 import UIKit
 
 final class MessagesViewController: UIViewController {
+  //MARK: private properties
+  private var message = ObjectMessage()
+  private var servise = FirestoreService()
   
-  
-  var conversationId = ""
+   var conversationId = ""
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
     ConversationManager.shared.observeMessages(conversationId: conversationId) { response in
-      
+      switch response {
+      case.success(let message):
+        guard let messageCount = message?.count else { return }
+        print("Coun\(messageCount)")
+      case.generalError, .noConnection:
+        print("Here is break!")
+        break
+      }
     }
   }
   
